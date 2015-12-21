0f7ed24f096865d059c2bbad94b886078ec57c71
apierce1992 <apierce1992@gmail.com>
2015-12-21 12:21:03 -0500

    sending text messages

---
+++ main.py
@@ -0,0 +1,12 @@
+import smtplib
+import os
+
+gmail_pass = os.environ["GMAILPASS"]
+number = os.environ["TEST_NUMBER"]
+
+server = smtplib.SMTP( "smtp.gmail.com", 587 )
+
+server.starttls()
+server.login( 'pushreminderapp@gmail.com', gmail_pass )
+
+server.sendmail( 'Skynet', number+'@vtext.com', 'This is an automated message from Push Reminder' )
